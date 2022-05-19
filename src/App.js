310bd707e9528b67237ca4869a9628aa13032682
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-05-19 18:06:33 +0530

    DISPLAYING COMPONENTS -imported components in app.js to render them

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h5> hello from main section</h5>
+      <TodoInput />
+      <TodoList />
     </div>
   );
 }
