5ef6b142db219b1f1ddc241defbcea882ad17926
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-05-19 18:02:56 +0530

    TODO COMPONENTS -TodoInput.js to input todos -TodoItem.js to dynamicaly update todo item -TodoList.js to render todo list

---
+++ src/components/TodoInput.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoInput = () => {
+  return (
+    <div>
+      TodoInput
+      <button className="btn">add to do</button>
+      <button className="btn btn-hipster btn-block">yolo</button>
+    </div>
+  );
+};
+
+export default TodoInput;
