04ef4a0c68550e6d7121c0e7a12e52367932072d
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-06-23 16:46:23 +0530

    modified todolist - display todoitem -added button to clear list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,26 @@
 import React from "react";
+import TodoItem from "./TodoItem";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  .btn {
+    width: 95%;
+    display: block;
+    margin: 0 auto;
+  }
+`;
 
 const TodoList = () => {
-  return <div>TodoList</div>;
+  return (
+    <Wrapper>
+      <ul>
+        <TodoItem />
+        <button type="button" className="btn btn-hipster">
+          clear list
+        </button>
+      </ul>
+    </Wrapper>
+  );
 };
 
 export default TodoList;
