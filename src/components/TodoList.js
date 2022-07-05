01984a1296197b8276c272f19d4de02de9b0ec46
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-07-05 17:27:59 +0530

    modified todoList.js - removed button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,24 +2,15 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
-  .btn {
-    width: 95%;
-    display: block;
-    margin: 0 auto;
-  }
-`;
+const TodoList = (props) => {
+  const { items } = props;
 
-const TodoList = () => {
   return (
-    <Wrapper>
-      <ul>
-        <TodoItem />
-        <button type="button" className="btn btn-hipster">
-          clear list
-        </button>
-      </ul>
-    </Wrapper>
+    <>
+      {items.map((item) => {
+        return <TodoItem key={item.id} {...item} />;
+      })}
+    </>
   );
 };
 
