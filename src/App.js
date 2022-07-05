43fc4ea1529fad92b790393224ddc08a33279e40
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-07-05 17:25:59 +0530

    modified app.js - included todo input fuctionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,89 @@
-import TodoList from "./components/TodoList";
-import TodoInput from "./components/TodoInput";
 import styled from "styled-components";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import TodoList from "./components/TodoList";
 
-const Wrapper = styled.div`
+const Wrapper = styled.section`
   h3 {
     text-align: center;
+  }
+
+  .todo {
+    border-radius: 3rem;
+    padding: 1rem;
+    border: transparent;
+    width: 100%;
+    height: 3rem;
+    margin: 0 auto;
+    text-align: left;
+    text-transform: capitalize;
+  }
+  .btn {
+    display: block;
+    margin: 0.5rem auto;
+
+    height: 2rem;
+  }
+  .btn-clear {
+    width: 95%;
+    display: block;
+    margin: 0 auto;
   }
 `;
 
 function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      // display alert
+    } else if (name && isEditing) {
+      // deal with editing
+    } else {
+      const id = uuidv4();
+      const newList = { id, title: name };
+      setList([...list, newList]);
+      setName("");
+    }
+  };
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const clearList = () => {
+    //show alert
+    setList([]);
+  };
+
   return (
-    <Wrapper>
-      <div className="container">
-        <h3>ToDo</h3>
-        <TodoInput />
-        <TodoList />
+    <Wrapper className="container">
+      <h3>ToDo</h3>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            name="todo"
+            value={name}
+            onChange={handleChange}
+            className="todo"
+            placeholder="add todo"
+          />
+          <button className="btn">add toDo</button>
+        </div>
+      </form>
+      <div>
+        <TodoList items={list} />
+        <button
+          type="button"
+          className="btn btn-clear btn-hipster"
+          onClick={clearList}
+        >
+          clear list
+        </button>
       </div>
     </Wrapper>
   );
