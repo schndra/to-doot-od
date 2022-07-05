4662e49f56158ba2175e738033addbe8df87bd0d
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-07-05 17:27:13 +0530

    modified todoIt.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import { FaTrashAlt } from "react-icons/fa";
-import { FaEdit } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
+import { FaRegEdit } from "react-icons/fa";
 
 const Wrapper = styled.div`
   background-color: var(--background-color);
@@ -9,9 +9,15 @@ const Wrapper = styled.div`
   padding: 0.6rem;
   margin-bottom: 0.5rem;
 
-  h5 {
+  .edit-btn {
+    margin-right: 0.5rem;
+  }
+
+  p {
     display: inline-block;
     margin: 0;
+    transition: var(--transition);
+    color: var(--grey-900);
   }
   li {
     list-style-type: none;
@@ -28,15 +34,20 @@ const Wrapper = styled.div`
   }
 `;
 
-const TodoItem = () => {
+const TodoItem = (props) => {
+  const { title } = props;
   return (
     <>
       <Wrapper>
         <li>
-          <h5>Title</h5>
+          <p>{title}</p>
           <div>
-            <FaEdit className="edit-link" />
-            <FaTrashAlt className="delete-btn" />
+            <button type="button" className="edit-btn">
+              {<FaRegEdit />}
+            </button>
+            <button type="button" className="delete-btn">
+              {<FaTrash />}
+            </button>
           </div>
         </li>
       </Wrapper>
